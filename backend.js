c3adae4c78fe74e83a421c7c0be63a790958c0b1
TCollins2001 <tc486@njit.edu>
2025-05-04 19:00:14 -0400

    ignore backend.js

--- backend.js
+++ backend.js
@@ -1,3 +1,4 @@
+/* 
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -253,3 +254,5 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+*/
